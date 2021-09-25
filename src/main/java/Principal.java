52adb12038bb43d5bbdb735d9b465beaa6e808b9
Rosy <msc_rmaria@accitesz.com>
2021-09-25 08:25:59 -0500

    mensaje en el caso 1 numeros romanos

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,19 +1,11 @@
 
 import javax.swing.JOptionPane;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
  class Principal {
      public static void main(String[] args) {
          int opcion=1;
-        
         do {
-           
-              
+             
         opcion= Integer.parseInt(JOptionPane.showInputDialog("Menu"
                 + "\n 1. Numeros en Romanos"
                 + "\n 2. Numeros en Inglés"
@@ -22,10 +14,10 @@ import javax.swing.JOptionPane;
                 + "\n 5. Imprimir Números en P'urepecha"
                 + "\n 8. Salir"
                 + "\n  Elige una opción"));
-        
-    //*    ImprimirNumeros in = new ImprimirNumeros();
+            //*    ImprimirNumeros in = new ImprimirNumeros();
         switch(opcion){
-            case 1:
+            case 1: 
+                    JOptionPane.showMessageDialog(null, "Elije una opción valida");
                 break;
             case 2:
                 break;
@@ -51,6 +43,5 @@ import javax.swing.JOptionPane;
         }
         }while(opcion!=8);
      }
- 
-    
+   
 }
